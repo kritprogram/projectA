7d29f434700b929c15bc5b7879ec38e2683257d6
kritprogram <kritprogram@gmail.com>
2018-04-19 22:25:00 +0300

    index

--- abc/src/main/java/com/gmail/kritprogram/hello/GreetingController.java
+++ abc/src/main/java/com/gmail/kritprogram/hello/GreetingController.java
@@ -5,13 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 
+    private static String lol = "abcd";
+
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
+        //lol = name;
         return "greeting";
     }
 
+    @RequestMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("name", lol);
+        return "index";
+    }
 }
