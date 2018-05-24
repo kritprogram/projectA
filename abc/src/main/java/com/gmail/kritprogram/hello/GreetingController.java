12df6feeecc3f70f9b66d6d1d3d55b4d55b2d921
kritprogram <kritprogram@gmail.com>
2018-05-25 00:42:40 +0300

    arduino fix

--- abc/src/main/java/com/gmail/kritprogram/hello/GreetingController.java
+++ abc/src/main/java/com/gmail/kritprogram/hello/GreetingController.java
@@ -4,7 +4,6 @@ import arduino.Arduino;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -19,23 +18,30 @@ public class GreetingController {
     @RequestMapping("/rfid")
     @ResponseBody
     public String getRfidData(Model model) {
-        return "-1057462234";
-        /*Arduino arduino = new Arduino("COM4", 9600);
-        boolean connected = arduino.openConnection();
-        System.out.println("Соединение установлено: " + connected);
-
-        String input = "";
-        while(input.isEmpty()){
-            input = arduino.serialRead();
+        try {
+            Arduino arduino = new Arduino("COM4", 9600);
+            boolean connected = arduino.openConnection();
+            System.out.println("Соединение установлено: " + connected);
+            String input = "";
+            int count = 0;
+            while (input.isEmpty()) {
+                input = arduino.serialRead();
+                count ++;
+                if (count >= 100){
+                    throw new Exception();
+                }
+                Thread.sleep(100);
+            }
+            arduino.closeConnection();
+            String[] nums = input.split("\n");
+            String hexNum = "FFFFFFFF";
+            for (int i = nums.length - 1; i >= 0; i--) {
+                hexNum = hexNum + nums[i];
+            }
+            System.out.println("Считал" + Long.parseUnsignedLong(hexNum, 16));
+            return "" + Long.parseUnsignedLong(hexNum, 16);
+        } catch (Exception e){
+            return "11111111";
         }
-        arduino.closeConnection();
-        String[] nums = input.split("\n");
-        String hexNum = "FFFFFFFF";
-        for (int i = nums.length - 1; i >= 0; i--) {
-            hexNum = hexNum + nums[i];
-        }
-
-        System.out.println("Считал" + Long.parseUnsignedLong(hexNum, 16))
-        return "" + Long.parseUnsignedLong(hexNum, 16);*/
     }
 }
--- abc/target/classes/static/js/index.js
+++ abc/target/classes/static/js/index.js
@@ -1,6 +1,6 @@
 /*
 Когда кликаем по кнопке log in выполняется функция авторизации
-создается get запрос со значением логина введенным пользователем на адрес сервера авторизации ГрГУ
+создается get запрос со значением логина введенным пользователем на адрес API сервера ГрГУ
 При успешном выполнении запроса в скрытое поле помещается ID пользователя; на месте поля для идентификации помещается приветсвие пользователя и кнопка logout
 ПО полученному ID пользователя создается get запрос на адрес сервера расписания студентов ГрГУ
 По полученным данным отресовываем таблицу с расписание авторизованного пользователя в блоке html-страницы расписание занятий
@@ -11,84 +11,71 @@ $(function () {
         //var params = encodeURI("?login="+ $('#login').val());
         var params = encodeURI("?login=jraga_mv_12");
         $
-        .get('https://api.grsu.by/1.x/app1/getStudent' + params)
-        .done(function (data) {
-            $('#tn').val(data.id);
-            $('#hello').html('Hello, ' + data.fullname);
-            $('#logout').removeClass('hidden');
-            $
-                    .get('https://api.grsu.by/1.x/app1/getGroupSchedule?studentId=' + $('#tn').val())
-                    .done(function (data) {
-                        $('.contentSchedule p').addClass('hidden');
-                        var $table = $('<table>').addClass('table')
-                                .append($('<tbody>'));
-                        data.days.forEach(function (element) {
-                            printLineWithDate($table, element.date)
-                            element.lessons.forEach(function (lesson) {
-                                printLine($table, lesson)
-                            })
-                        }, this);
-                        $('.contentSchedule').append($table);
-                    });
-        })
-        .fail(function () {
-            $('.contentSchedule p').removeClass('hidden');
-        });
-    });
-/*
-Обработчик на нажатие кнопки rfid. Вызываем собственный API который возвращает значение id rfid метки.
-Создается get запрос со значением id RFID метки на адрес сервера авторизации ГрГУ
-При успешном выполнении запроса в скрытое поле помещается ID пользователя; на месте поля для идентификации помещается приветсвие пользователя и кнопка logout
-ПО полученному ID пользователя создается get запрос на адрес сервера расписания студентов ГрГУ
-По полученным данным отресовываем таблицу с расписание авторизованного пользователя в блоке html-страницы расписание занятий
-Если пользователь не авторизован - в блоке расписане занятий отображается сообщение о необходимости идентификации
-*/
-    $('#rfid').on('click', function () {
-        $.getJSON("/rfid", null, function (item) {
-            $.getJSON('https://api.grsu.by/1.x/app3/getStudentAllByCard?cardid=' + item)
-			.done(function (data) {
-			    $('#tn').val(data.TN);
-                    $('#hello').html('Hello, ' + data.FIO);
-                    $('#logout').removeClass('hidden');
-                    $
-                        .get('https://api.grsu.by/1.x/app1/getGroupSchedule?studentId=' + $('#tn').val())
-                        .done(function (data) {
-                            $('.contentSchedule p').addClass('hidden');
-                            var $table = $('<table>').addClass('table')
-                                    .append($('<tbody>'));
-                            data.days.forEach(function (element) {
-                                printLineWithDate($table, element.date)
-                                element.lessons.forEach(function (lesson) {
-                                    printLine($table, lesson)
-                                })
-                            }, this);
-                            $('.contentSchedule').append($table);
-                        });
-				})
+            .get('https://api.grsu.by/1.x/app1/getStudent' + params)
+            .done(function (data) {
+                authorization (data);
+            })
             .fail(function () {
                 $('.contentSchedule p').removeClass('hidden');
             });
-		});
+    });
+    /*
+    Обработчик на нажатие кнопки rfid. Вызываем собственный API который возвращает значение id rfid метки.
+    Создается get запрос со значением id RFID метки на адрес API сервера ГрГУ
+    При успешном выполнении запроса в скрытое поле помещается ID пользователя; на месте поля для идентификации помещается приветсвие пользователя и кнопка logout
+    ПО полученному ID пользователя создается get запрос на адрес сервера расписания студентов ГрГУ
+    По полученным данным отресовываем таблицу с расписание авторизованного пользователя в блоке html-страницы расписание занятий
+    Если пользователь не авторизован - в блоке расписане занятий отображается сообщение о необходимости идентификации
+    */
+    $('#rfid').on('click', function () {
+        $.getJSON("/rfid", null, function (item) {
+            $.getJSON('https://api.grsu.by/1.x/app3/getStudentAllByCard?cardid=' + item)
+                .done(function (data) {
+                    authorization (data);
+                })
+                .fail(function () {
+                    $('.contentSchedule p').removeClass('hidden');
+                });
+        });
     });
 
-    var printLineWithDate = function ($table, tDate) {
-        $table
-            .append(
-                $('<tr>')
-                    .addClass('success')
-                    .append($('<td>').html(tDate))
-            );
+    var authorization = function (data) {
+        if (data.id !== 0) {
+            $('#tn').val(data.id);
+            $('#hello').html('<div class="greetings">Hello, ' + data.fullname + '</div>');
+            $('#logout').removeClass('hidden');
+            $
+                .get('https://api.grsu.by/1.x/app1/getGroupSchedule?studentId=' + $('#tn').val())
+                .done(function (data) {
+                    $('.message').addClass('hidden');
+                    var $table = $('<table>').addClass('table')
+                        .append($('<tbody>'));
+                    data.days.forEach(function (element) {
+                        $table
+                            .append(
+                                $('<tr>')
+                                    .addClass('success')
+                                    .append($('<td>').html(element.date))
+                            );
+                        element.lessons.forEach(function (lesson) {
+                            $table
+                                .append(
+                                    $('<tr>')
+                                        .append($('<td>').html(lesson.timeStart + ' - ' + lesson.timeEnd))
+                                        .append($('<td>').html(lesson.type + ' ' + lesson.title))
+                                        .append($('<td>').html(lesson.address + ': ' + lesson.room))
+                                        .append($('<td>').html(lesson.teacher.fullname))
+                                );
+                        })
+                    }, this);
+                    $('.contentSchedule').append($table);
+                });
+        }
+        else {
+            alert("Проверьте корректность вводимых данных")
+        }
     }
-    var printLine = function ($table, lesson) {
-        $table
-        .append(
-            $('<tr>')
-                .append($('<td>').html(lesson.timeStart + ' - ' + lesson.timeEnd))
-                .append($('<td>').html(lesson.type + ' ' + lesson.title))
-                .append($('<td>').html(lesson.address + ': ' + lesson.room))
-                .append($('<td>').html(lesson.teacher.fullname))
-            );
-    }
+
     /*
     Функция увеличения размера блока по которому произведен клик
     */
